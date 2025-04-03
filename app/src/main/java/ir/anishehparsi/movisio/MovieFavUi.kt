127d701113ba060974ef199fae08306f731dcf95
alimotamedkia <info@andishehparsi.ir>
2025-04-03 08:43:41 +0330

    update MovieFavUi with delete Fav Item

--- app/src/main/java/ir/anishehparsi/movisio/MovieFavUi.kt
+++ app/src/main/java/ir/anishehparsi/movisio/MovieFavUi.kt
@@ -3,8 +3,6 @@ package ir.anishehparsi.movisio
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,12 +18,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.orhanobut.hawk.Hawk
 import com.ramcosta.composedestinations.annotation.Destination
 import ir.anishehparsi.movisio.Model.Result
@@ -45,13 +40,25 @@ fun MovieFavUi(modifier: Modifier = Modifier) {
             .padding(4.dp)
     ) {
         items(movieFavList ?: emptyList()) {
-            SaveMovieList(item = it)
+            SaveMovieList(item = it,
+                onDeletedCLicked = {
+                    val updatedList: List<Result>? = Hawk.get("movieFavList", emptyList())
+                    val finalList = updatedList?.toMutableList()
+                    finalList?.remove(it)
+                    Hawk.put("movieFavList", finalList)
+                    movieFavList = Hawk.get("movieFavList", emptyList())
+                })
+
         }
     }
 }
 
 @Composable
-fun SaveMovieList(modifier: Modifier = Modifier, item: Result) {
+fun SaveMovieList(
+    modifier: Modifier = Modifier,
+    item: Result,
+    onDeletedCLicked: () -> Unit,
+) {
     val context = LocalContext.current
     OutlinedCard(
         modifier = Modifier
@@ -69,6 +76,7 @@ fun SaveMovieList(modifier: Modifier = Modifier, item: Result) {
             Image(
                 modifier = Modifier
                     .clickable {
+                        onDeletedCLicked()
                         Toast.makeText(context, "Favorite Canceled ❌", Toast.LENGTH_SHORT).show()
                     },
                 painter = painterResource(R.drawable.favorite_border),

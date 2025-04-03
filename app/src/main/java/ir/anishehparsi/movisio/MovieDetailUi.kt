62ab271d13fe64a710755138bcd5dd2c98b45d5f
alimotamedkia <info@andishehparsi.ir>
2025-04-03 08:35:13 +0330

    update MovieFavUi

--- app/src/main/java/ir/anishehparsi/movisio/MovieDetailUi.kt
+++ app/src/main/java/ir/anishehparsi/movisio/MovieDetailUi.kt
@@ -1,6 +1,8 @@
 package ir.anishehparsi.movisio
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,20 +15,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.orhanobut.hawk.Hawk
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import ir.anishehparsi.movisio.Model.Images
 import ir.anishehparsi.movisio.Model.Result
 
+
 @Destination
 @Composable
-fun MovieDetailUi(modifier: Modifier = Modifier, item: Result, images: Images) {
-
+fun MovieDetailUi(
+    modifier: Modifier = Modifier,
+    item: Result,
+    images: Images
+) {
+val context = LocalContext.current
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -40,10 +50,12 @@ fun MovieDetailUi(modifier: Modifier = Modifier, item: Result, images: Images) {
             error = painterResource(R.drawable.movisio_nbg)
         )
 
-        Text(text = item.title,
+        Text(
+            text = item.title,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center)
+            textAlign = TextAlign.Center
+        )
         Spacer(modifier.height(16.dp))
         Row(
             modifier = Modifier
@@ -55,8 +67,21 @@ fun MovieDetailUi(modifier: Modifier = Modifier, item: Result, images: Images) {
             )
             Spacer(Modifier.width(4.dp))
             Text(text = item.vote_average.toString())
-            Spacer(modifier.weight(0.5f))
+            Spacer(modifier.weight(1f))
             Text(text = item.original_language)
+            Spacer(modifier.weight(1f))
+            Image(
+                modifier = Modifier
+                    .clickable {
+                        val dbList: List<Result>? = Hawk.get("movieFavList", emptyList())
+                        val finalList = dbList?.toMutableList()
+                        finalList?.add(item)
+                        Hawk.put("movieFavList", finalList)
+                        Toast.makeText(context, "Movie saved 👍", Toast.LENGTH_SHORT).show()
+                    },
+                painter = painterResource(R.drawable.favorite),
+                contentDescription = ""
+            )
         }
         Spacer(modifier.height(32.dp))
         Text(text = item.overview)
--- app/src/main/java/ir/anishehparsi/movisio/MovieFavUi.kt
+++ app/src/main/java/ir/anishehparsi/movisio/MovieFavUi.kt
@@ -1,11 +1,81 @@
 package ir.anishehparsi.movisio
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.orhanobut.hawk.Hawk
 import com.ramcosta.composedestinations.annotation.Destination
+import ir.anishehparsi.movisio.Model.Result
 
 @Destination
 @Composable
 fun MovieFavUi(modifier: Modifier = Modifier) {
+    var movieFavList by remember { mutableStateOf<List<Result>?>(null) }
 
+    LaunchedEffect(key1 = Unit) {
+        movieFavList = Hawk.get("movieFavList", emptyList())
+    }
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(4.dp)
+    ) {
+        items(movieFavList ?: emptyList()) {
+            SaveMovieList(item = it)
+        }
+    }
+}
+
+@Composable
+fun SaveMovieList(modifier: Modifier = Modifier, item: Result) {
+    val context = LocalContext.current
+    OutlinedCard(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+        ) {
+            Text(
+                text = item.title,
+            )
+            Spacer(modifier.weight(1f))
+            Image(
+                modifier = Modifier
+                    .clickable {
+                        Toast.makeText(context, "Favorite Canceled ❌", Toast.LENGTH_SHORT).show()
+                    },
+                painter = painterResource(R.drawable.favorite_border),
+                contentDescription = ""
+            )
+        }
+
+    }
+    Spacer(modifier.height(4.dp))
 }
